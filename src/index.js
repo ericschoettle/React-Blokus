6c70f486dd7e73637fbdd78259cb23a3df63d919
ericschoettle <eric.schoettle@gmail.com>
2018-08-01 11:24:19 -0700

    Playing with data structure

--- src/index.js
+++ src/index.js
@@ -8,40 +8,46 @@ let boardSize = 20;
 function Square(props) {
     return (
         <button
-            className={"square player" + props.playernumber}
+            className={`square player${props.playernumber || ''}`}
             onClick={props.onClick}
         >
         </button>
     );
 }
 
-function Row(props) {
-    const squares = [];
-    for (let j = 0; j < props.squares.length; j++) {
-        squares.push(<Square 
-            key ={j} 
-            onClick={() => props.onClick(j, props.rowIndex)}
-            playernumber = {props.squares[j]}
-        />)
+class Row extends React.Component {
+    render () {
+        // debugger; // And here this.props.squares isn't updating with what was passed in from Board
+        const squares = [];
+        for (let j = 0; j < this.props.squares.length; j++) {
+            squares.push(<Square 
+                key ={j} 
+                onClick={() => this.props.onClick(j, this.props.rowIndex)}
+                playernumber = {this.props.squares[j]} // Naming here is temporary - passing the value in player number, rather than the whole object, to make testing easier
+            />)
+        }
+        return (
+            <div className="board-row">
+                {squares}
+            </div>
+        )
     }
-    return (
-        <div className="board-row">
-            {squares}
-        </div>
-    )
 }
 
 
 class Board extends React.Component {
     render() {
-        const rows = [];
+        let rows = [];
         for (let i = 0; i < this.props.rows.length; i++) {
             rows.push(<Row 
                 key ={i} 
                 rowIndex = {i} 
-                squares = {this.props.rows[i].squares}
+                squares = {this.props.rows[i].squares.map(square => {
+                    return {playerNumber: square.playerNumber};
+                })}
                 onClick = {this.props.onClick}/>)
-        }
+        } 
+        // debugger // here rows has the updated value for squares
         return (
             <div>
                 {rows}
@@ -68,7 +74,7 @@ class Game extends React.Component {
         super(props);
         this.state = {
             history: [{
-                rows: Array(boardSize).fill({squares: Array(boardSize).fill(null)}),
+                rows: Array(boardSize).fill({squares: Array(boardSize).fill({playerNumber: null, pieceIndex: null })}),
                 // players: play
             }],
             pieces: makePieces({
@@ -79,30 +85,32 @@ class Game extends React.Component {
             xIsNext: true,
         }
     }
-    erasePiece(piece) {
-
-
-    }
 
     // takes a piece and draws it on the board, in terms of rows/columns
-    drawPiece(piece) {
+    drawPieces(pieces) {
         const history = this.state.history.slice(0,this.state.stepNumber + 1);
-        const current = history[this.state.stepNumber];
-        let newCurrent = []
-
-        piece.cells.forEach(cell => {
-            const xCoord = piece.centerX + cell[0];
-            const yCoord = piece.centerY + cell[1];
-            newCurrent = update(current, {
-                rows: {[yCoord]:{squares:{[xCoord]:{$set: piece.playerNumber}}}}
+        let current = history[this.state.stepNumber];
+        pieces.forEach(piece => {
+            piece.cells.forEach(cell => {
+                const xCoord = piece.centerX + cell[0];
+                const yCoord = piece.centerY + cell[1];
+                current = update(current, {
+                    rows: {[yCoord]:{squares:{[xCoord]:{$set: {playerNumber: piece.playerNumber, pieceIndex: piece.pieceIndex}}}}}
+                });
             });
-        });
+        })
         
         this.setState({
-            history: history.concat([newCurrent]),
+            history: history.concat([current]),
             stepNumber: history.length,
         })
     }
+    componentDidMount() {
+        // initialize board
+        const pieces = this.state.pieces;
+        this.drawPieces(pieces)
+    }
+
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
@@ -142,7 +150,6 @@ class Game extends React.Component {
         );
     }
     handleClick(centerX, centerY) {
-        debugger;
         // const history = this.state.history.slice(0,this.state.stepNumber + 1);
         // const current = history[this.state.stepNumber];
         // const squares = current.rows.slice();
@@ -151,7 +158,7 @@ class Game extends React.Component {
         piece[0].centerX = centerX;
         piece[0].centerY = centerY;
 
-        this.drawPiece(piece[0]);
+        this.drawPieces([piece[0]]);
 
 
 
